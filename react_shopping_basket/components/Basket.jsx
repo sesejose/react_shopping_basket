bd28de0184375a132cebe163d3e0d05acf441e6b
José <sesejose@gmail.com>
2022-11-11 09:43:24 +0100

    5cart total works

--- react_shopping_basket/components/Basket.jsx
+++ react_shopping_basket/components/Basket.jsx
@@ -1,10 +1,23 @@
 export default function Basket(props) {
+  // When we click in the button in Product.jsx we call addProduct() that adds a item/cart to the basket.
+  // Here in the basket we MAP that cart to get the name, price and amount.
+  // When a new cart is added getTotal() runs.
+  // Then TOTAL is updated with the return of that function.
+
+  function getTotal() {
+    let total = 0;
+    props.cart.forEach((item) => {
+      total += item.amount * item.price;
+    });
+    return total;
+  }
+
   return (
     <>
       <fieldset className="Basket">
         <legend>Basket</legend>
         <div>
-          <div>Products in the basket:</div>
+          {/* <div>Products in the basket:</div> */}
           <ul>
             {props.cart.map((item) => (
               <li>
@@ -12,6 +25,7 @@ export default function Basket(props) {
               </li>
             ))}
           </ul>
+          <h3>Total: {getTotal()},-</h3>
         </div>
       </fieldset>
     </>
